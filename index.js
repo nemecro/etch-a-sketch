d0a087b492a23bbac7733e62835d8df50267e1ff
nemecro <nemecstudies@gmail.com>
2024-10-10 10:18:20 +0200

    fix: draw grid with gridsize row and gridsize column amounts

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ let squareContainerInfo = squareContainer.getBoundingClientRect();
 
 // write a function that will draw certain amount of squares
 // based on the user input or DEFAULT 16x16
-const drawSquares = function (gridsize = 16){
-    // get proportions into variables
-    let proportions = squareContainerInfo.width / (gridsize);
-    // based on the grid size create amount of squares
-    for (let i = 0; i < gridsize * gridsize; i++){
-        const square = document.createElement('div');
-        square.textContent = i +1;
-        square.classList.add('square');
-        // based on the amount of square, calculate the width and height of each square
-        // width is equal to - parent element width / number of gridsize
-        square.style = `width: ${proportions}px; height: ${proportions}px;`;
-        squareContainer.append(square);
-    }
+const drawGrid = function (gridsize = 16){
+    for (let i = 0; i < gridsize; i++){
+        const row = document.createElement('div');
+        row.classList.add('row');
+        squareContainer.appendChild(row);
+        for (let j = 0; j < gridsize; j++){
+            const column = document.createElement('div');
+            column.classList.add('column');
+            column.style = `width: ${squareContainerInfo.width/gridsize}px; height: ${squareContainerInfo.width/gridsize}px;`;
+            column.textContent = i + 1;
+            row.appendChild(column);
+        }
+    }   
 }
 
-drawSquares();
+drawGrid();
